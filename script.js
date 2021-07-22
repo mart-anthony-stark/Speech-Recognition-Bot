f42da06a1ecd1065f4dfc8e075e67ab2d92772d1
Mart Anthony Salazar <69393826+mart-anthony-stark@users.noreply.github.com>
2021-07-22 13:10:19 +0000

    speech synthesis

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btnFace = document.querySelector('#btn span')
 const textView = document.querySelector('h3.text')
 const recognition = new SpeechRecognition()
 
+recognition.lang = 'en-US'
+
 recognition.onstart= () => {
   console.log('Listening...')
   btnFace.classList.add('speaking')
@@ -15,9 +17,11 @@ recognition.onresult = (e) =>{
   const text = e.results[0][0].transcript
   console.log(text)
   textView.innerText = '' + text
-
-  if(text.includes('hello'))
-    console.log('Good day sir')
+  
+  if(text.includes('hello')){
+    // let utterance = new SpeechSynthesisUtterance("Good day. Sir!");
+    // speechSynthesis.speak(utterance);
+  }
 }
 
 recognition.onaudioend = () => {
@@ -27,5 +31,8 @@ recognition.onaudioend = () => {
 }
 
 btn.addEventListener('click', () => {
+  if(btnFace.classList.contains('speaking')){
+    return recognition.stop()
+  }
   recognition.start()
 })
